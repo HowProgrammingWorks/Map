14588259481f8e7324e7381a00c77c60c47d10e0
tshemsedinov <timur.shemsedinov@gmail.com>
2017-11-20 03:16:58 +0200

    Add basic examples

---
+++ JavaScript/1-array.js
@@ -0,0 +1,50 @@
+'use strict';
+
+class Dictionary {
+  constructor() {
+    this.map = {};
+  }
+  set(key, value) {
+    return this.map[key] = value;
+  }
+  get(key) {
+    return this.map[key];
+  }
+  has(key) {
+    return this.map.hasOwnProperty(key);
+  }
+  delete(key) {
+    delete this.map[key];
+  }
+  get size() {
+    return Object.keys(this.map).length;
+  }
+  keys() {
+    return Object.keys(this.map);
+  }
+  clear() {
+    this.map = {};
+  }
+}
+
+// Usage
+
+const cityPopulation = new Dictionary();
+
+cityPopulation.set('Shanghai', 24256800);
+cityPopulation.set('Beijing',  21516000);
+cityPopulation.set('Delhi',    16787941);
+cityPopulation.set('Lagos',    16060303);
+
+cityPopulation.delete('Shanghai');
+
+if (cityPopulation.has('Beijing')) {
+  console.log('Beijing:', cityPopulation.get('Beijing'));
+}
+
+if (!cityPopulation.has('Shanghai')) {
+  console.log('no data for Shanghai');
+}
+
+console.log('size:', cityPopulation.size);
+console.log('keys:', cityPopulation.keys());
