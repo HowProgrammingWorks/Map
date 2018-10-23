57ca7b585feb8184fc94cc1990f4aaa87404ec07
tshemsedinov <timur.shemsedinov@gmail.com>
2018-10-23 23:43:21 +0300

    Remove .toString()

--- JavaScript/5-index.js
+++ JavaScript/5-index.js
@@ -3,8 +3,7 @@
 const fs = require('fs');
 
 const getDataset = file => {
-  const lines = fs.readFileSync(file, 'utf8')
-    .toString().split('\n');
+  const lines = fs.readFileSync(file, 'utf8').split('\n');
   lines.shift();
   lines.pop();
   return lines.map(line => line.split(','));
